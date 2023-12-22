f94c0415cc58b4c2fe6c55eb66519509425ada0a
AxelanO7 <jeremia123.jm@gmail.com>
2023-12-22 16:24:07 +0800

    feat (nav) : logout

--- src/layouts/navbar.tsx
+++ src/layouts/navbar.tsx
@@ -1,51 +1,52 @@
 import React from "react";
 
 export default function Navbar() {
+  const handleLogout = () => {
+    localStorage.removeItem("token");
+    window.location.href = "/login";
+  };
+
   return (
     <div className="flex flex-col fixed top z-[1] w-full bg-slate-100">
       <nav className="flex p-4 items-center">
         <div className="w-16" />
-        <>
-          <div className="bg-success p-2 rounded">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 24 24"
-              fill="white"
-              className="w-6 h-6"
-            >
-              <path
-                fillRule="evenodd"
-                d="M3 6.75A.75.75 0 013.75 6h16.5a.75.75 0 010 1.5H3.75A.75.75 0 013 6.75zM3 12a.75.75 0 01.75-.75h16.5a.75.75 0 010 1.5H3.75A.75.75 0 013 12zm0 5.25a.75.75 0 01.75-.75h16.5a.75.75 0 010 1.5H3.75a.75.75 0 01-.75-.75z"
-                clipRule="evenodd"
-              />
-            </svg>
-          </div>
-          <div className="w-4" />
-          <input
-            placeholder="Search for something..."
-            className="bg-transparent"
-          />
-        </>
+        <div className="bg-success p-2 rounded">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+            fill="white"
+            className="w-6 h-6"
+          >
+            <path
+              fillRule="evenodd"
+              d="M3 6.75A.75.75 0 013.75 6h16.5a.75.75 0 010 1.5H3.75A.75.75 0 013 6.75zM3 12a.75.75 0 01.75-.75h16.5a.75.75 0 010 1.5H3.75A.75.75 0 013 12zm0 5.25a.75.75 0 01.75-.75h16.5a.75.75 0 010 1.5H3.75a.75.75 0 01-.75-.75z"
+              clipRule="evenodd"
+            />
+          </svg>
+        </div>
+        <div className="w-4" />
+        <input
+          placeholder="Search for something..."
+          className="bg-transparent"
+        />
         <div className="grow" />
-        <>
-          <div className="flex">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 24 24"
-              fill="gray"
-              className="w-6 h-6"
-            >
-              <path
-                fillRule="evenodd"
-                d="M7.5 3.75A1.5 1.5 0 006 5.25v13.5a1.5 1.5 0 001.5 1.5h6a1.5 1.5 0 001.5-1.5V15a.75.75 0 011.5 0v3.75a3 3 0 01-3 3h-6a3 3 0 01-3-3V5.25a3 3 0 013-3h6a3 3 0 013 3V9A.75.75 0 0115 9V5.25a1.5 1.5 0 00-1.5-1.5h-6zm10.72 4.72a.75.75 0 011.06 0l3 3a.75.75 0 010 1.06l-3 3a.75.75 0 11-1.06-1.06l1.72-1.72H9a.75.75 0 010-1.5h10.94l-1.72-1.72a.75.75 0 010-1.06z"
-                clipRule="evenodd"
-              />
-            </svg>
-            <div className="w-4" />
-            <p className="text-gray-500">Log Out</p>
-            <div className="w-4" />
-          </div>
-        </>
+        <div className="flex" onClick={handleLogout}>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+            fill="gray"
+            className="w-6 h-6"
+          >
+            <path
+              fillRule="evenodd"
+              d="M7.5 3.75A1.5 1.5 0 006 5.25v13.5a1.5 1.5 0 001.5 1.5h6a1.5 1.5 0 001.5-1.5V15a.75.75 0 011.5 0v3.75a3 3 0 01-3 3h-6a3 3 0 01-3-3V5.25a3 3 0 013-3h6a3 3 0 013 3V9A.75.75 0 0115 9V5.25a1.5 1.5 0 00-1.5-1.5h-6zm10.72 4.72a.75.75 0 011.06 0l3 3a.75.75 0 010 1.06l-3 3a.75.75 0 11-1.06-1.06l1.72-1.72H9a.75.75 0 010-1.5h10.94l-1.72-1.72a.75.75 0 010-1.06z"
+              clipRule="evenodd"
+            />
+          </svg>
+          <div className="w-4" />
+          <p className="text-gray-500">Log Out</p>
+          <div className="w-4" />
+        </div>
       </nav>
     </div>
   );
